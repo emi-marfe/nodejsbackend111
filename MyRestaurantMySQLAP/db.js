f3a63a3d8fd69b2c18692a375f850a70ac043aff
emi-marfe <tohbee4real@gmail.com>
2019-09-03 11:18:54 -0700

    commit message

---
+++ MyRestaurantMySQLAP/db.js
@@ -0,0 +1,11 @@
+﻿var config = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        port: 3306,
+        db: 'myrestaurant'
+    }
+}
+
+module.exports = config
